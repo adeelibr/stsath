98ee6afd0d5c1426e80cdecd5645d4abe8fcb74d
adeelibr <adeelimranr@gmail.com>
2016-12-22 06:37:59 +0500

    index.ejs route commented because it used index.html in the public folder

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,10 +3,10 @@ var router = express.Router();
 
 var api = require('./api');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'STSATH' });
-});
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'STSATH' });
+// });
 
 router.use('/api', api);
 
