afafdd9548b57e72c64c33e10b097a970af23f79
adeelibr <adeelimranr@gmail.com>
2016-12-22 05:54:04 +0500

    Bootstrap Sass Integrated

--- client/app.jsx
+++ client/app.jsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router'
 
 // import TodoApp from 'TodoApp';
-// require('style!css!sass!./styles/app.scss');
+require('style!css!sass!./styles/app.sass');
 
 let App = React.createClass({
   render: function () {
     return (
-      <div>
-        <p className="container">Hello</p>
+      <div className="fluid-container">
+        <p>Hello</p>
       </div>
     );
   }
