ccfdcaccac66f807dc5d3e6f7dd9bedf5d4c0fe0
Adeel Imran <adeelimranr@gmail.com>
2016-12-27 14:14:08 +0500

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,9 +1,9 @@
 module.exports = {
   secret: 'somerandomkeywordwillchangeitifIwantto',
   twitter: {
-    consumer_key: 'IAmBCgTiyRwrzfh0iQnkjGiWD',
-    consumer_secret: 'tFoFfan7ZNfIAI4QPPt2ftauURaymxfunEeTsuXId76aMAUXvp',
-    access_token: '17391021-vMLRNOgjYuEJUkwhJZIX2drWTyeGarDjQ8zy8GAVm',
-    access_token_secret: 'FhbZ6ZFfyxbOxZ9kxEDifhmMjZjsTymhnCguZ1pkeWgbI'
+    consumer_key: 'something',
+    consumer_secret: 'something',
+    access_token: 'something',
+    access_token_secret: 'something'
   }
 };
