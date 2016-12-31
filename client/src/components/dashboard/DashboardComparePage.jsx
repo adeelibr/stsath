7c90bff9ed29bf96d7d278fe2a5fee9a4f65b24d
adeelibr <adeelimranr@gmail.com>
2016-12-31 05:18:11 +0500

    Dashbpard Search & Compare Pages, Forms Added In Them

--- client/src/components/dashboard/DashboardComparePage.jsx
+++ client/src/components/dashboard/DashboardComparePage.jsx
@@ -1,11 +1,77 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-let DashboardComparePage = (props) => {
-  return (
-    <div className="">
-      <p>Lets do some comparing.</p>
-    </div>
-  );
-};
+import SearchIcon from 'material-ui/svg-icons/action/youtube-searched-for';
+
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+
+class DashboardComparePage extends Component {
+
+  constructor (props) {
+    super(props);
+    this.state = {
+      errors: {},
+      choice: {
+        choice1: '',
+        choice2: '',
+      },
+    };
+  }
+
+  changeInfo = (e) => {
+    const field = e.target.name;
+    const choice = this.state.choice;
+    choice[field] = e.target.value;
+    this.setState({ choice });
+  }
+
+  render () {
+    let { errors, choice } = this.state;
+
+    return (
+      <div className="">
+        <h3><SearchIcon /> Perfom Sentiment On Two Choices</h3>
+        <form action="/">
+
+          <div className="row">
+            <div className="col-md-5 col-sm-4">
+              <TextField
+                floatingLabelText="Choice #2"
+                name="choice1"
+                onChange={this.changeInfo}
+                errorText={errors.choice1}
+                value={choice.choice1}
+                fullWidth={true}
+              />
+            </div>
+
+            <div className="col-md-5 col-sm-4">
+              <TextField
+                floatingLabelText="Choice #2"
+                name="choice2"
+                onChange={this.changeInfo}
+                errorText={errors.choice2}
+                value={choice.choice2}
+                fullWidth={true}
+              />
+            </div>
+
+            <div className="col-md-2 col-sm-4">
+              <RaisedButton
+                type="submit"
+                label="Perform Sentiment"
+                className="inline-form-button"
+                primary={true}
+                fullWidth={true}
+              />
+            </div>
+          </div>
+
+        </form>
+      </div>
+    );
+  }
+
+}
 
 export default DashboardComparePage;
--- client/src/components/dashboard/DashboardSearchPage.jsx
+++ client/src/components/dashboard/DashboardSearchPage.jsx
@@ -1,11 +1,61 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { Link } from 'react-router';
 
-let DashboardSearchPage = (props) => {
-  return (
-    <div className="">
-      <p>Lets do some searching.</p>
-    </div>
-  );
-};
+import SearchIcon from 'material-ui/svg-icons/action/youtube-searched-for';
+
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+
+class DashboardSearchPage extends Component {
+
+  constructor (props) {
+    super(props);
+    this.state = {
+      errors: {},
+      searchQuery: '',
+    };
+  }
+
+  changeInfo = (e) => {
+    const searchQuery = e.target.value;
+    this.setState({ searchQuery });
+  }
+
+  render () {
+    let { errors, searchQuery } = this.state;
+
+    return (
+      <div className="">
+        <h3><SearchIcon /> Search</h3>
+        <form action="/">
+
+          <div className="row">
+            <div className="col-md-10 col-sm-8">
+              <TextField
+                floatingLabelText="Search Tweets & Hashtags"
+                name="search"
+                onChange={this.changeInfo}
+                errorText={errors.searchQuery}
+                value={searchQuery}
+                fullWidth={true}
+              />
+            </div>
+            <div className="col-md-2 col-sm-4">
+              <RaisedButton
+                type="submit"
+                label="Perform Sentiment"
+                className="inline-form-button"
+                primary={true}
+                fullWidth={true}
+              />
+            </div>
+          </div>
+
+        </form>
+      </div>
+    );
+  }
+
+}
 
 export default DashboardSearchPage;
