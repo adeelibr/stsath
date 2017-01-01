13e3718847a5c930515b4ec02aa4287f4032e7f1
adeelibr <adeelimranr@gmail.com>
2017-01-01 06:37:20 +0500

    choice controller functionality completed

--- server/controllers/search.js
+++ server/controllers/search.js
@@ -3,66 +3,82 @@ var sentimental = require('Sentimental');
 
 var config = require('../../config');
 
+var twitter = new twit({
+  consumer_key: config.twitter.consumer_key,
+  consumer_secret: config.twitter.consumer_secret,
+  access_token: config.twitter.access_token,
+  access_token_secret: config.twitter.access_token_secret
+});
+
 module.exports = {
 
   choice: function (req, res, next) {
     var choiceOne = req.query.choice_one;
     var choiceTwo = req.query.choice_two;
-    // Options Provided By Client
-    var choices = [ choiceOne, choiceTwo ];
-    // Establish Connection With Twitter
-    var twitter = new twit({
-      consumer_key: config.twitter.consumer_key,
-      consumer_secret: config.twitter.consumer_secret,
-      access_token: config.twitter.access_token,
-      access_token_secret: config.twitter.access_token_secret
+
+    // var today = new Date();
+    // var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+
+    let choiceOneScore = 0;
+    let choiceTwoScore = 0;
+    let choiceOneTweets = [];
+    let choiceTwoTweets = [];
+
+    let highestScore = -Infinity;
+    let highestChoice = null;
+
+    getTweets(choiceOne)
+    .then((res) => {
+      let statuses = res.data.statuses;
+      choiceOneTweets = statuses;
+      choiceOneScore = performAnalysis(choiceOneTweets);
+      return getTweets(choiceTwo)
+    })
+    .then((res) => {
+      let statuses = res.data.statuses;
+      choiceTwoTweets = statuses;
+      choiceTwoScore = performAnalysis(choiceTwoTweets);
+    })
+    .then(() => {
+      if (choiceOneScore > choiceTwoScore) {
+        highestScore = choiceOneScore;
+        highestChoice = choiceOne
+      } else {
+        highestScore = choiceTwoScore;
+        highestChoice = choiceTwo;
+      }
+    })
+    .then(() => {
+      res.status(200).json({
+        success: true,
+        choiceOneScore,
+        choiceTwoScore,
+        highestScore,
+        highestChoice,
+        choiceOneTweets,
+        choiceTwoTweets
+      }).end();
+    })
+    .catch((err) => {
+      console.log('Error is: ', err.message);
     });
 
-    var highestScore = -Infinity;
-    var highestChoice = null;
-
-    var today = new Date();
-    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-    var array = [];
-    var score = 0;
-
-    var choiceOneArray = [];
-    var choiceTwoArray = [];
-
-    // Iterate Through The Choices Array From The Request
-    for (var i=0; i < choices.length; i++) {
-      // Immediatly invoked function - IIFE
-      (function(i) {
-        array.push(choices[i]);
-        twitter.get('search/tweets', {q: '' + choices[i] + ' since:' + date, count: 4}, function(err, data) {
-          // Person Sentimental Anaylysis
-          score = performanceAnalysis(data['statuses']);
-          console.log('Choice: ' + choices[i] + ' Score: ' + score);
-          if (score > highestScore) {
-            highestScore = score;
-            highestChoice = choices[i];
-            console.log('Winner Is: ', highestChoice);
-          }
-        });
-      })(i);
-    }
-
-    // setTimeout
-    setTimeout(function() {
-        res.status(200).json({
-          success: true,
-          message: "Choices results found",
-          choices: choices,
-          choicesTweets: [{choiceOneArray, choiceTwoArray}],
-          score: highestScore,
-          highestChoice: highestChoice,
-        }).end();
-    }, 5000);
   }
 
 }; // end of module.exports
 
-performanceAnalysis = function(tweetSet) {
+const getTweets = function (choice) {
+  return twitter.get('search/tweets', {q: '' + choice, count: 100}, function(err, data) {
+    if (err) {
+      const reason = new Error(err);
+      return Promise.reject(reason);
+    } else {
+      var statuses = data['statuses'];
+      return Promise.resolve(statuses);
+    }
+  });
+};
+const performAnalysis = function (tweetSet) {
   // For Every Choice Initialize tweetSet results to 0
   var results = 0;
   for (var i=0; i < tweetSet.length; i++) {
