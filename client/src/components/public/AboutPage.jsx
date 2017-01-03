ac39f5f0693460ffc4be2d7c64e028edd0bda5a9
adeelibr <adeelimranr@gmail.com>
2017-01-03 11:42:45 +0500

    Home page done, About page in progress

--- client/src/components/public/AboutPage.jsx
+++ client/src/components/public/AboutPage.jsx
@@ -1,11 +1,136 @@
 import React from 'react';
 
-let AboutPage = (props) => {
-  return (
-    <div>
-      <p>This is about awesome</p>
-    </div>
-  );
-};
+import {
+  Step,
+  Stepper,
+  StepButton,
+  StepContent,
+} from 'material-ui/Stepper';
+import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
 
+import Paper from 'material-ui/Paper';
+import Avatar from 'material-ui/Avatar';
+
+class AboutPage extends React.Component {
+
+  state = {
+    stepIndex: 0,
+  };
+
+  handleNext = () => {
+    const {stepIndex} = this.state;
+    if (stepIndex < 2) {
+      this.setState({stepIndex: stepIndex + 1});
+    }
+  };
+
+  handlePrev = () => {
+    const {stepIndex} = this.state;
+    if (stepIndex > 0) {
+      this.setState({stepIndex: stepIndex - 1});
+    }
+  };
+
+  renderStepActions(step) {
+    return (
+      <div style={{margin: '12px 0'}}>
+        {(step !== 2) && (<RaisedButton
+          label="Next"
+          disableTouchRipple={true}
+          disableFocusRipple={true}
+          primary={true}
+          onTouchTap={this.handleNext}
+          style={{marginRight: 12}}
+        />)}
+        {step > 0 && (
+          <FlatButton
+            label="Back"
+            disableTouchRipple={true}
+            disableFocusRipple={true}
+            onTouchTap={this.handlePrev}
+          />
+        )}
+      </div>
+    );
+  }
+
+  render() {
+    const {stepIndex} = this.state;
+
+    return (
+      <div className="col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4">
+        <Stepper
+          activeStep={stepIndex}
+          linear={false}
+          orientation="vertical"
+        >
+          <Step>
+            <StepButton onTouchTap={() => this.setState({stepIndex: 0})}>
+              About Us
+            </StepButton>
+            <StepContent>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+              {this.renderStepActions(0)}
+            </StepContent>
+          </Step>
+          <Step>
+            <StepButton onTouchTap={() => this.setState({stepIndex: 1})}>
+              Motivation
+            </StepButton>
+            <StepContent>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+              {this.renderStepActions(1)}
+            </StepContent>
+          </Step>
+          <Step>
+            <StepButton onTouchTap={() => this.setState({stepIndex: 2})}>
+              Reason Behind {' '} <b>STSATH</b>
+            </StepButton>
+            <StepContent>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+              {this.renderStepActions(2)}
+            </StepContent>
+          </Step>
+        </Stepper>
+
+        <div className="row">
+          <div className="col-sm-2">
+            <Avatar
+              src="http://pbs.twimg.com/profile_background_images/593573080959569921/Zz2vEaj7.png"
+              size={80}
+            />
+          </div>
+          <div className="col-sm-10">
+            Hi, I am a software engineeer, and I am awesome
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 export default AboutPage;
--- client/src/components/public/HomePage.jsx
+++ client/src/components/public/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { AutoRotatingCarousel, Slide } from 'material-auto-rotating-carousel'
-import { green400, green600, blue400, blue600, red400, red600 } from 'material-ui/styles/colors'
+import { indigo400, indigo600, blue400, blue600, cyan400, cyan600 } from 'material-ui/styles/colors'
 
 
 let HomePage = (props) => {
@@ -16,8 +16,8 @@ let HomePage = (props) => {
     >
       <Slide
         media={<img src="http://www.icons101.com/icon_png/size_256/id_79394/youtube.png" />}
-        mediaBackgroundStyle={{ backgroundColor: red400 }}
-        contentStyle={{ backgroundColor: red600 }}
+        mediaBackgroundStyle={{ backgroundColor: blue400 }}
+        contentStyle={{ backgroundColor: blue600 }}
         title="This is a very cool feature"
         subtitle="Just using this will blow your mind."
       />
@@ -30,8 +30,8 @@ let HomePage = (props) => {
       />
       <Slide
         media={<img src="http://www.icons101.com/icon_png/size_256/id_76704/Google_Settings.png" />}
-        mediaBackgroundStyle={{ backgroundColor: green400 }}
-        contentStyle={{ backgroundColor: green600 }}
+        mediaBackgroundStyle={{ backgroundColor: blue400 }}
+        contentStyle={{ backgroundColor: blue600 }}
         title="May the force be with you"
         subtitle="The Force is a metaphysical and ubiquitous power in the Star Wars universe."
       />
