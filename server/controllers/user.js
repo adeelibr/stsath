82120cb5a7bc64f403b6ec9fb80b60641c1958d7
adeelibr <adeelimranr@gmail.com>
2017-01-05 23:46:19 +0500

    getuserbyid, updateuserinfo api

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,7 @@
+const validator = require('validator');
+
 var models = require('../models');
-var users = models.users;
+var Users = models.users;
 
 module.exports = {
 
@@ -11,14 +13,14 @@ module.exports = {
 			return res.status(400).json({ success: false, message: "Parameter is missing" }).end();
 		}
 
-		users.findOne({
+		Users.findOne({
 			where: { id : id }
 		})
-		.then(function(user) {
+		.then((user) => {
 			if(user === null) {
 				return res.status(400).json({ success: false, message: "No User Found" }).end();
 			}
-			return res.status(200).json({ success: true, user: user }).end();
+			return res.status(200).json({ success: true, user }).end();
 		})
 		.catch(function(err) {
 			console.log(err);
@@ -26,19 +28,84 @@ module.exports = {
 		})
 	}, // end of getUser
 
-	delete: function (req, res, next) {
-			return res.status(200).send({ "user": "u" }).end();
-	},
+	updateUserInfo: function (req, res, next) {
+		const validateResult = validateUpdateUserInfoFormBody(req.body)
+		if (!validateResult.success) {
+      return res.status(400).json({
+        success: false,
+        message: validateResult.message,
+        errors: validateResult.errors
+      }).end();
+    }
 
-	getAllUser: function (req, res, next) {
-		users.findAll({})
-		.then(function(users) {
-			if(users === null) {
-				return res.status(200).json({ success: false, message: "No Users Exist" }).end();
+		let user = req.body;
+		let id = req.params.id;
+
+		Users.update(user, {
+			where: {
+				id: id
 			}
-			return res.status(200).json({ success: true, users: users }).end();
+		})
+		.then((data) => {
+			return Users.findOne({ where: { id : id } });
+		})
+		.then((data) => {
+			return res.status(200).json({ success: true, message: 'Succesfully updated account', user: data }).end();
+		})
+		.catch((error) => {
+			console.log(err);
+			return res.status(500).json({ success: false, message: "Internal Server Error" }).end();
 		})
 	},
 
+	delete: function (req, res, next) {
+		return res.status(200).send({ "user": "u" }).end();
+	},
 
+	getAllUser: function (req, res, next) {
+		Users.findAll({})
+		.then((users) => {
+			if(users === null) {
+				return res.status(200).json({ success: false, message: "No Users Exist" }).end();
+			}
+			return res.status(200).json({ success: true, users }).end();
+		})
+	},
+
+}
+
+function validateUpdateUserInfoFormBody(payload) {
+  const errors = {};
+  let isFormValid = true;
+  let message = '';
+
+  if (!payload || typeof payload.username !== 'string' || payload.username.trim().length === 0) {
+    isFormValid = false;
+    errors.username = 'Please provide your Username';
+  }
+
+  if (!payload || typeof payload.first_name !== 'string' || payload.first_name.trim().length === 0) {
+    isFormValid = false;
+    errors.first_name = 'Please provide your First Name';
+  }
+
+  if (!payload || typeof payload.last_name !== 'string' || payload.last_name.trim().length === 0) {
+    isFormValid = false;
+    errors.last_name = 'Please provide your Last Name';
+  }
+
+  if (!payload || typeof payload.email !== 'string' || !validator.isEmail(payload.email) ) {
+    isFormValid = false;
+    errors.email = 'Please provide your Email';
+  }
+
+  if (!isFormValid) {
+    message = 'Check the form for errors';
+  }
+
+  return {
+    success: isFormValid,
+    message,
+    errors
+  }
 }
--- server/routes/api/user.js
+++ server/routes/api/user.js
@@ -3,11 +3,14 @@ var router = express.Router();
 
 var userFunc = require('../../controllers/user');
 
-// @endpoint api/user/ Get all user
-router.get('/', userFunc.getAllUser);
-
 // @endpoint api/user/:id Get a user by id
 router.get('/:id', userFunc.getUser);
+
+// @endpoint api/user/:id Update user info
+router.post('/:id', userFunc.updateUserInfo);
+
+// @endpoint api/user/ Get all user
+router.get('/', userFunc.getAllUser);
 
 // @endpoint api/user/:id Delete a user by id
 router.delete('/:id', userFunc.delete);
