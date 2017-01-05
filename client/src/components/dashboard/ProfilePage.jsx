64a9e09f6c824ecca4aa13c8b91fe8074c19bb16
adeelibr <adeelimranr@gmail.com>
2017-01-05 05:08:38 +0500

    ProfileUpdateForm component added to the profile page

--- client/src/components/dashboard/ProfilePage.jsx
+++ client/src/components/dashboard/ProfilePage.jsx
@@ -1,11 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-let ProfilePage = (props) => {
-  return (
-    <div className="">
-      <p>Profile Page</p>
-    </div>
-  );
+import ProfileUpdateForm from './components/ProfileUpdateForm';
+
+class ProfilePage extends Component {
+
+  constructor (props) {
+    super(props);
+    this.state = {
+      errors: {},
+      user: {
+        username: '',
+        first_name: '',
+        last_name: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+      }
+    };
+  }
+
+  processForm = (e) => {
+    let {user} = this.state;
+    e.preventDefault();
+    // console.log('user: ', user);
+
+    // SignupAPI(user)
+    // .then((res) =>{
+    //   if (!res.success) {
+    //     const errors = res.errors ? res.errors : {};
+    //     errors.summary = res.message;
+    //     this.setState({ errors });
+    //   } else {
+    //     // console.log('Form is valid, Account Has Been Created');
+    //     let snackbar = this.state.snackbar;
+    //     snackbar.open = true;
+    //     this.setState({ errors: {}, snackbar });
+    //   }
+    // });
+  }
+
+  changeUser = (e) => {
+    const field = e.target.name;
+    const user = this.state.user;
+    user[field] = e.target.value;
+    this.setState({ user });
+  }
+
+  render () {
+    let {errors, user} = this.state;
+
+    return (
+      <div className="row">
+        <div className="col-md-10 col-md-offset-1">
+          <ProfileUpdateForm
+            onSubmit={this.processForm}
+            onChange={this.changeUser}
+            errors={errors}
+            user={user}
+            />
+        </div>
+      </div>
+    );
+  }
+
 };
 
 export default ProfilePage;
