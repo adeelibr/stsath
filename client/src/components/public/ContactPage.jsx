79ee170b6ef44e454936b0a5b03e9bbbc07d1839
adeelibr <adeelimranr@gmail.com>
2017-01-07 18:05:38 +0500

    Working on making contact form

--- client/src/components/public/ContactPage.jsx
+++ client/src/components/public/ContactPage.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-let ContactPage = (props) => {
-  return (
-    <div>
-      <p>This is contact awesome</p>
-    </div>
-  );
+export default class ContactPage extends Component {
+
+  render () {
+    return (
+      <div classMame="row">
+        <div className="col-md-6 col-md-offset-3">
+          <p>This is contact awesome</p>
+        </div>
+      </div>
+    );
+  }
+
 };
 
-export default ContactPage;
+// export default ContactPage;
