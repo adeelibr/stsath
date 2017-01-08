8b1c2d26f0e08445f3bf8c0fa1724f56274aae16
adeelibr <adeelimranr@gmail.com>
2017-01-08 13:45:42 +0500

    Updated dummy.js with email configuration parameters which are set up by nodemailer module

--- config/dummy.js
+++ config/dummy.js
@@ -1,3 +1,5 @@
+// Just rename this file to index.js and put your credentials here
+
 module.exports = {
 
   secret: '********************************',
@@ -6,6 +8,16 @@ module.exports = {
     consumer_secret: '********************************',
     access_token: '********************************',
     access_token_secret: '********************************'
+  },
+  emailConnection: {
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // use SSL
+    logger: true,  // Logger
+    auth: {
+      user: '********',
+      pass: '********'
+    }
   }
 
 };
