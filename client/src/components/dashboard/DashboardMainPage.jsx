cbb446166cbe2d03942629f1d3f126d83810c63c
adeelibr <adeelimranr@gmail.com>
2017-01-08 19:07:56 +0500

    Dashboard Main Page complete

--- client/src/components/dashboard/DashboardMainPage.jsx
+++ client/src/components/dashboard/DashboardMainPage.jsx
@@ -3,7 +3,21 @@ import React from 'react';
 let DashboardMainPage = (props) => {
   return (
     <div className="">
-      <p>This is main login page bypassed awesome</p>
+      <div className="jumbotron">
+        <div className="container">
+          <h1>Hi there,</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+            in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum.</p>
+          <br/><br/>
+          <p>Cheers,</p>
+          <p><b>STSATH Team</b></p>
+        </div>
+      </div>
     </div>
   );
 };
