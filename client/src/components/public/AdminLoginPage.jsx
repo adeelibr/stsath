b0764c799d46c3364837d0072a0217739187805d
adeelibr <adeelimranr@gmail.com>
2017-01-15 02:05:29 +0500

    Added AdminLoginPage, TOdo: Api integration left

---
+++ client/src/components/public/AdminLoginPage.jsx
@@ -0,0 +1,69 @@
+import React, {Component} from 'react';
+
+import LoginForm from 'public/components/LoginForm';
+import LoginAPI from 'LoginAPI';
+
+class AdminLoginPage extends Component {
+
+  constructor (props) {
+    super(props);
+    this.state = {
+      errors: {},
+      user: {
+        username: '',
+        password: '',
+      }
+    };
+
+    this.processForm = this.processForm.bind(this);
+    this.changeUser = this.changeUser.bind(this);
+  }
+
+  processForm (e) {
+    let {user} = this.state;
+    e.preventDefault();
+    // console.log('username: ', this.state.user.username);
+    // console.log('password: ', this.state.user.password);
+
+    LoginAPI(user)
+    .then((res) =>{
+      if (!res.success) {
+        const errors = res.errors ? res.errors : {};
+        errors.summary = res.message;
+        this.setState({ errors });
+      } else {
+        // console.log('Form is valid');
+        this.setState({ errors: {} });
+        localStorage.setItem('admintoken', res.token);
+        this.props.router.push('/admin');
+      }
+    });
+  }
+
+  changeUser (e) {
+    const field = e.target.name;
+    const user = this.state.user;
+    user[field] = e.target.value;
+    this.setState({ user });
+  }
+
+  render () {
+    let {errors, user} = this.state;
+
+    return (
+      <div className="row">
+        <div className="col-md-4 col-md-offset-4">
+          <LoginForm
+            onSubmit={this.processForm}
+            onChange={this.changeUser}
+            errors={errors}
+            user={user}
+          />
+        </div>
+      </div>
+    );
+  }
+
+};
+
+export default AdminLoginPage;
