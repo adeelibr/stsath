782f04814e91a79d52e11a8b0c2cb86fc0fc3e5d
adeelibr <adeelimranr@gmail.com>
2017-01-16 12:43:11 +0500

    Feedbacks Table Improved, Delete Dialog Added

--- client/src/components/admin/AdminViewFeedbacksPage.jsx
+++ client/src/components/admin/AdminViewFeedbacksPage.jsx
@@ -3,14 +3,35 @@ import moment from 'moment'
 
 import FeedbackAPI from 'FeedbackAPI';
 
+import {Dialog, FlatButton, IconButton} from 'material-ui';
+
+import FingerprintIcon from 'material-ui/svg-icons/action/fingerprint';
+import ArchiveIcon from 'material-ui/svg-icons/content/archive';
+import DeleteIcon from 'material-ui/svg-icons/action/delete';
+
+let styles = {
+  icon: {
+    // top: 0,
+    marginTop: -24, marginBottom: -24, marginLeft: 2, marginRight: 2,
+    padding: 0,
+    heigth: 24,
+    width: 24,
+  }
+}
+
 export default  class AdminViewFeedbackPage extends Component {
 
   constructor (props) {
     super(props);
     this.state = {
-      feedbacks: {}
+      feedbacks: {},
+      deleteDialogOpen: false,
     };
   }
+
+  handleDeleteDialogOpen = () => { this.setState({deleteDialogOpen: true}); };
+
+  handleDeleteDialogClose = () => { this.setState({deleteDialogOpen: false}); };
 
   componentDidMount () {
     FeedbackAPI.getAllFeedbacks()
@@ -36,7 +57,7 @@ export default  class AdminViewFeedbackPage extends Component {
     let renderFeedbacks = () => {
       return feedbacks.map((feedback, i) => {
         return (
-          <tr key={i}>
+          <tr key={i} className="text-lowercase">
             <td>{feedback.id}</td>
             <td>{feedback.person_name}</td>
             <td>{feedback.rating}</td>
@@ -46,33 +67,71 @@ export default  class AdminViewFeedbackPage extends Component {
             <td>{feedback.siteRecommend}</td>
             <td>{feedback.competitors ? feedback.competitors : 'None'}</td>
             <td>{moment(feedback.createdAt).format('MM/DD/YYYY')}</td>
-            <td>[More Details] [Archive] [Delete]</td>
+            <td>
+              <IconButton touch={true} style={styles.icon}>
+                <FingerprintIcon />
+              </IconButton>
+              <IconButton touch={true} style={styles.icon}>
+                <ArchiveIcon />
+              </IconButton>
+              <IconButton touch={true} style={styles.icon} onClick={this.handleDeleteDialogOpen}>
+                <DeleteIcon />
+              </IconButton>
+            </td>
           </tr>
         )
       });
     };
 
+    let renderDeleteDialog = () => {
+      const actions = [
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onTouchTap={this.handleDeleteDialogClose}
+      />,
+      <FlatButton
+        label="Delete"
+        primary={true}
+        onTouchTap={this.handleDeleteDialogClose}
+      />,
+    ];
+      return (
+        <Dialog
+            title="Delete"
+            actions={actions}
+            modal={true}
+            open={this.state.deleteDialogOpen}
+          >
+            Are You Sure You Want To Delete This Feedback ?
+        </Dialog>
+      );
+    };
+
     return (
-      <div className="table-responsive">
-        <table className="table table-hover">
-          <thead>
-            <tr>
-              <th>Id</th>
-              <th>Person Name</th>
-              <th>Rating</th>
-              <th>Review</th>
-              <th>Site Usefulness</th>
-              <th>Site Design</th>
-              <th>Site Recommend</th>
-              <th>Any Competitors</th>
-              <th>Created At</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {feedbacks.length > 0 ? renderFeedbacks() : renderEmptyRow() }
-          </tbody>
-        </table>
+      <div>
+        <div className="table-responsive">
+          <table className="table table-hover table-striped">
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>Person Name</th>
+                <th>Rating</th>
+                <th>Review</th>
+                <th>Site Usefulness</th>
+                <th>Site Design</th>
+                <th>Site Recommend</th>
+                <th>Any Competitors</th>
+                <th>Created At</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {feedbacks.length > 0 ? renderFeedbacks() : renderEmptyRow() }
+            </tbody>
+          </table>
+        </div>
+        {renderDeleteDialog()}
       </div>
     );
   }
